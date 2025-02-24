f966a4dfb5e34bce38aad46fab2d46212de5cdfe
Tejaswi G <65135962+tejaswigoli@users.noreply.github.com>
2025-02-24 14:39:22 +0000

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "azs_info" {
+  value = data.aws_availability_zones.available
+}
+
+  output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
--- variables.tf
+++ variables.tf
@@ -18,3 +18,59 @@ variable "common_tags" {
   type = map
   default = {}
 }
+variable "vpc_tags" {
+  default = {}
+}
+variable "igw_tags" {
+  default = {}
+}
+variable "public_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.public_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid public cidr" 
+  }
+}
+variable "public_subnet_tags" {
+  default = {}
+}
+variable "private_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.private_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private cidr" 
+  }
+}
+variable "private_subnet_tags" {
+  default = {}
+}
+variable "database_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid database cidr" 
+  }
+}
+variable "database_subnet_tags" {
+  default = {}
+}
+variable "nat_gateway_tags" {
+  default = {}
+}
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+variable "database_route_table_tags" {
+    default = {}
+}
+variable "is_peering_required" {
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+}
